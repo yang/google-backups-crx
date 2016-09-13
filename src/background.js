2de547985853a041bee361ea017a1c11089f65a4
Yang Zhang <yaaang@gmail.com>
2016-09-13 09:14:07 -0700

    Clean up

--- src/background.js
+++ src/background.js
@@ -5,7 +5,8 @@ function waitForAuthOrDownload(theTabId, i) {
       console.log('tab loaded', tabId, changeInfo, tab)
       chrome.tabs.onUpdated.removeListener(onTabs);
       chrome.tabs.executeScript(tabId, {file: "src/content_script.js"}, () => {
-        chrome.tabs.executeScript(tabId, {code: 'setTimeout(tryLogin, 1000)'}, () => {});
+        // Wait a bit for good measure.
+        chrome.tabs.executeScript(tabId, {code: 'setTimeout(login, 1000)'}, () => {});
       });
     }
   });
@@ -15,13 +16,13 @@ function waitForAuthOrDownload(theTabId, i) {
     chrome.downloads.onChanged.addListener(function onDownloaded(download) {
       if (download.endTime && theDownload.id === download.id) {
         console.log('done!');
-        injectTheScript(i + 1);
+        autoDownload(i + 1);
       }
     });
   });
 }
 
-function injectTheScript(i) {
+function autoDownload(i) {
   chrome.tabs.create({url: 'https://takeout.google.com/settings/takeout/downloads'}, (theTab) => {
     chrome.tabs.onUpdated.addListener(function onTabs(tabId, changeInfo, tab) {
       if (tabId === theTab.id && changeInfo.status === 'complete' && tab.url === 'https://takeout.google.com/settings/takeout/downloads') {
@@ -37,5 +38,5 @@ function injectTheScript(i) {
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  injectTheScript(0);
+  autoDownload(0);
 });
--- src/content_script.js
+++ src/content_script.js
@@ -1,7 +1,9 @@
-function getLeafs(txt, visible) {
+function getLeafs(txt, requireVisible) {
+  // Find the leaf nodes that are direct parents of the matching text,
+  // optionally requiring them to be visible (tested with positive offsetTop).
   return Array.from(document.getElementsByTagName('div'))
     .filter(
-      x => (!visible || x.offsetTop > 0) &&
+      x => (!requireVisible || x.offsetTop > 0) &&
         Array.from(x.childNodes)
           .filter(child => child.nodeType == x.TEXT_NODE && x.innerText === txt)
           .length > 0
@@ -11,23 +13,17 @@ function getLeafs(txt, visible) {
 function getDownload(i) {
   if (getLeafs('Download', true).length == 0)
     getLeafs('Show exports', false)[0].click();
-  setTimeout((() => {
+  // Doesn't instantaneously show.
+  setTimeout(function() {
     if (i >= getLeafs('Download', true).length)
-      alert('Done!');
+      alert('Done with all the Google Downloads!');
     else
       getLeafs('Download', true)[i].click();
-  }), 1000);
+  }, 1000);
 }
 
-function tryLogin() {
+function login() {
   let passwd = document.getElementById('Passwd');
   passwd.value = 'XXX FILL ME IN';
   passwd.form.submit();
-  return;
-  // This autofill is not visible for security.
-  if (passwd.value.length > 0) {
-    passwd.form.submit();
-  } else {
-    console.log('waiting for passwd', passwd);
-  }
 }
