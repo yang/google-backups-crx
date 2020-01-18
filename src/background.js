0335ab2a36852262b4b2274bc18d3230cd9034c5
Yang Zhang <yaaang@gmail.com>
2020-01-18 09:47:40 -0800

    Reformat with prettier

--- src/background.js
+++ src/background.js
@@ -1,15 +1,25 @@
 function waitForAuthOrDownload(theTabId, i) {
   var onTabs;
-  chrome.tabs.onUpdated.addListener(onTabs = function(tabId, changeInfo, tab) {
-    if (tabId === theTabId && changeInfo.status === 'complete') {
-      console.log('tab loaded', tabId, changeInfo, tab)
-      chrome.tabs.onUpdated.removeListener(onTabs);
-      chrome.tabs.executeScript(tabId, {file: "src/content_script.js"}, () => {
-        // Wait a bit for good measure.
-        chrome.tabs.executeScript(tabId, {code: 'setTimeout(login, 1000)'}, () => {});
-      });
-    }
-  });
+  chrome.tabs.onUpdated.addListener(
+    (onTabs = function(tabId, changeInfo, tab) {
+      if (tabId === theTabId && changeInfo.status === "complete") {
+        console.log("tab loaded", tabId, changeInfo, tab);
+        chrome.tabs.onUpdated.removeListener(onTabs);
+        chrome.tabs.executeScript(
+          tabId,
+          { file: "src/content_script.js" },
+          () => {
+            // Wait a bit for good measure.
+            chrome.tabs.executeScript(
+              tabId,
+              { code: "setTimeout(login, 1000)" },
+              () => {}
+            );
+          }
+        );
+      }
+    })
+  );
   chrome.downloads.onCreated.addListener(function onDownloading(theDownload) {
     chrome.downloads.onCreated.removeListener(onDownloading);
     chrome.tabs.onUpdated.removeListener(onTabs);
@@ -17,12 +27,12 @@ function waitForAuthOrDownload(theTabId, i) {
       if (theDownload.id === download.id && download.state) {
         console.log(download);
         switch (download.state.current) {
-          case 'complete':
+          case "complete":
             console.log(`completed ${i}!`);
             autoDownload(i + 1);
             break;
-          case 'interrupted':
-            console.log(`retrying ${i}`)
+          case "interrupted":
+            console.log(`retrying ${i}`);
             autoDownload(i);
             break;
           default:
@@ -35,28 +45,53 @@ function waitForAuthOrDownload(theTabId, i) {
 
 function autoDownload(i, retry = 0) {
   if (retry > 5) return;
-  chrome.tabs.create({url: 'https://takeout.google.com/settings/takeout/downloads'}, (theTab) => {
-    chrome.tabs.onUpdated.addListener(function onTabs(tabId, changeInfo, tab) {
-      if (tabId === theTab.id && changeInfo.status === 'complete' && tab.url === 'https://takeout.google.com/settings/takeout/downloads') {
-        chrome.tabs.onUpdated.removeListener(onTabs);
-        chrome.tabs.executeScript(tab.id, {file: "src/content_script.js"}, function() {
-          console.log('autoDownload executeScript(content_script)', arguments);
-          // Sometimes I get an error, "cannot access contents of the page".
-          // No idea why.
-          if (chrome.runtime.lastError) {
-            return setTimeout(() => autoDownload(i, retry + 1), 10000);
-          }
-          chrome.tabs.executeScript(tab.id, {code: `getDownload(${i})`}, function() {
-            console.log('autoDownload executeScript(getDownload)', arguments);
-            if (chrome.runtime.lastError) {
-              return setTimeout(() => autoDownload(i, retry + 1), 10000);
+  chrome.tabs.create(
+    { url: "https://takeout.google.com/settings/takeout/downloads" },
+    theTab => {
+      chrome.tabs.onUpdated.addListener(function onTabs(
+        tabId,
+        changeInfo,
+        tab
+      ) {
+        if (
+          tabId === theTab.id &&
+          changeInfo.status === "complete" &&
+          tab.url === "https://takeout.google.com/settings/takeout/downloads"
+        ) {
+          chrome.tabs.onUpdated.removeListener(onTabs);
+          chrome.tabs.executeScript(
+            tab.id,
+            { file: "src/content_script.js" },
+            function() {
+              console.log(
+                "autoDownload executeScript(content_script)",
+                arguments
+              );
+              // Sometimes I get an error, "cannot access contents of the page".
+              // No idea why.
+              if (chrome.runtime.lastError) {
+                return setTimeout(() => autoDownload(i, retry + 1), 10000);
+              }
+              chrome.tabs.executeScript(
+                tab.id,
+                { code: `getDownload(${i})` },
+                function() {
+                  console.log(
+                    "autoDownload executeScript(getDownload)",
+                    arguments
+                  );
+                  if (chrome.runtime.lastError) {
+                    return setTimeout(() => autoDownload(i, retry + 1), 10000);
+                  }
+                  waitForAuthOrDownload(tab.id, i);
+                }
+              );
             }
-            waitForAuthOrDownload(tab.id, i);
-          });
-        });
-      }
-    });
-  });
+          );
+        }
+      });
+    }
+  );
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
--- src/content_script.js
+++ src/content_script.js
@@ -1,27 +1,27 @@
 function getLeafs(txt, requireVisible) {
   // Find the leaf nodes that are direct parents of the matching text,
   // optionally requiring them to be visible (tested with positive offsetTop).
-  return Array.from(document.querySelectorAll('*'))
-    .filter(
-      x => (!requireVisible || x.offsetTop > 0) &&
-        (x.innerText || '').toLowerCase() == txt.toLowerCase() &&
-        [...x.childNodes].filter(child => child.nodeType == x.TEXT_NODE).length > 0
-    )
+  return Array.from(document.querySelectorAll("*")).filter(
+    x =>
+      (!requireVisible || x.offsetTop > 0) &&
+      (x.innerText || "").toLowerCase() == txt.toLowerCase() &&
+      [...x.childNodes].filter(child => child.nodeType == x.TEXT_NODE).length >
+        0
+  );
 }
 
 function getDownload2(i) {
   // Doesn't instantaneously show.
-  if (getLeafs('Download', true).length < 3)
+  if (getLeafs("Download", true).length < 3)
     setTimeout(() => getDownload2(i), 1000);
-  else if (i >= getLeafs('Download', true).length)
-    alert('Done with all the Google Downloads!');
-  else
-    getLeafs('Download', true)[i].click();
+  else if (i >= getLeafs("Download", true).length)
+    alert("Done with all the Google Downloads!");
+  else getLeafs("Download", true)[i].click();
 }
 
 function getDownload(i) {
-  if (getLeafs('Download', true).length == 0)
-    getLeafs('Show exports', false)[0].click();
+  if (getLeafs("Download", true).length == 0)
+    getLeafs("Show exports", false)[0].click();
   getDownload2(i);
 }
 
@@ -30,12 +30,12 @@ function login() {
   if (!passwd) {
     setTimeout(login, 1000);
   } else {
-    passwd.value = 'XXX FILL ME IN';
-    console.log('filled in');
+    passwd.value = "XXX FILL ME IN";
+    console.log("filled in");
     //passwd.form.submit();
-    var submit = document.getElementById('passwordNext') ||
-      document.querySelector('input[type="submit"]')
-    ;
+    var submit =
+      document.getElementById("passwordNext") ||
+      document.querySelector('input[type="submit"]');
     submit.click();
   }
 }
